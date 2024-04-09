58ed0ed20463789f120a1426dbf294593fa66883
Roy Buhay <roydesign@outlook.com>
2024-04-09 17:11:39 +0800

    revamp: version 1.2

---
+++ src/data/projects.ts
@@ -0,0 +1,189 @@
+export const projects = [
+  {
+    title: "zeiton",
+    desc: "Zeiton is a reward scoring unit that allows people who are enthusiastic to serve the society to receive substantial rewards, thereby promoting everyone to participate in social construction activities.",
+    width: 8,
+    pageWidth: 10,
+    path: "zeiton-logo.svg",
+    roles: ["front-end", "back-end", "UI designer/developer"],
+    technologies: ["ionic", "angular", "typescript", "sass", "sketch"],
+    screens: ["messages.webp", "task.webp", "home.webp"],
+    screenshots: [
+      {
+        path: "login.jpg",
+        class: "mobile",
+      },
+      {
+        path: "home.webp",
+        class: "mobile",
+      },
+      {
+        path: "messages.webp",
+        class: "mobile",
+      },
+      {
+        path: "task.webp",
+        class: "mobile",
+      },
+      {
+        path: "map.jpg",
+        class: "mobile",
+      },
+      {
+        path: "details.png",
+        class: "mobile",
+      },
+    ],
+  },
+  {
+    title: "avazoo",
+    desc: "Avazoo allows users to use local and national chains to receive additional entries by posting in social media platforms information about the causes important to organizations and the raffle.",
+    width: 5,
+    pageWidth: 6,
+    path: "avazoo-logo.png",
+    roles: ["front-end", "back-end", "UI designer/developer"],
+    technologies: [
+      "ReactNative",
+      "expo",
+      "typescript",
+      "laravel",
+      "mysql",
+      "tailwind",
+      "figma",
+    ],
+    screens: ["shares2.jpg", "popup2.jpg", "home2.jpg"],
+    screenshots: [
+      {
+        path: "home2.jpg",
+        class: "mobile",
+      },
+      {
+        path: "popup2.jpg",
+        class: "mobile",
+      },
+      {
+        path: "shares2.jpg",
+        class: "mobile",
+      },
+      {
+        path: "profile2.jpg",
+        class: "mobile",
+      },
+      {
+        path: "reward2.jpg",
+        class: "mobile",
+      },
+    ],
+  },
+  {
+    title: "matchit",
+    desc: "Zeiton is a reward scoring unit that allows people who are enthusiastic to serve the society to receive substantial rewards, thereby promoting everyone to participate in social construction activities.",
+    width: 5,
+    pageWidth: 6,
+    path: "avazoo-logo.png",
+    roles: ["front-end", "back-end", "UI designer/developer"],
+    technologies: [
+      "react",
+      "typescript",
+      "laravel",
+      "mysql",
+      "tailwind",
+      "figma",
+    ],
+    screens: ["2.jpg", "5-2.jpg", "1-2.jpg"],
+    screenshots: [
+      {
+        path: "1-2.jpg",
+        class: "mobile",
+      },
+      {
+        path: "2.jpg",
+        class: "mobile",
+      },
+      {
+        path: "3.jpg",
+        class: "mobile",
+      },
+      {
+        path: "4.jpg",
+        class: "mobile",
+      },
+      {
+        path: "5-2.jpg",
+        class: "mobile",
+      },
+    ],
+  },
+  {
+    title: "crateclub",
+    desc: "Zeiton is a reward scoring unit that allows people who are enthusiastic to serve the society to receive substantial rewards, thereby promoting everyone to participate in social construction activities.",
+    width: 10,
+    pageWidth: 12,
+    path: "crateclub-logo.svg",
+    roles: ["front-end", "back-end", "UI designer/developer"],
+    technologies: ["laravel", "sass", "javascript", "mysql", "figma", "stripe"],
+    screens: ["details.jpg", "subs.jpg", "home.jpg"],
+    screenshots: [
+      {
+        path: "login2.jpg",
+        class: "mobile",
+      },
+      {
+        path: "home.jpg",
+        class: "mobile",
+      },
+      {
+        path: "subs.jpg",
+        class: "mobile",
+      },
+      {
+        path: "store.jpg",
+        class: "mobile",
+      },
+      {
+        path: "details.jpg",
+        class: "mobile",
+      },
+      {
+        path: "storedetails.jpg",
+        class: "mobile",
+      },
+    ],
+  },
+  {
+    title: "crypto",
+    desc: "Zeiton is a reward scoring unit that allows people who are enthusiastic to serve the society to receive substantial rewards, thereby promoting everyone to participate in social construction activities.",
+    width: 13,
+    pageWidth: 15,
+    path: "crypto-logo2.svg",
+    roles: ["front-end", "back-end", "UI designer/developer"],
+    technologies: [
+      "nextjs",
+      "typescript",
+      "firebase",
+      "solidity",
+      "hardhat",
+      "tailwind",
+      "figma",
+    ],
+    screens: ["home.jpg", "mobile2.jpg", "mobile1.jpg"],
+    screenshots: [
+      {
+        path: "home-desktop.jpg",
+        class: "desktop",
+      },
+      {
+        path: "home-bottom.jpg",
+        class: "desktop",
+      },
+      {
+        path: "mobile1.jpg",
+        class: "mobile",
+      },
+      {
+        path: "mobile2.jpg",
+        class: "mobile",
+      },
+    ],
+  },
+];
---
+++ src/store/store.ts
@@ -0,0 +1,3 @@
+import { atom } from "nanostores";
+
+export const $page = atom<string>("zeiton");
